8e0b849626670464a66a75a14b44460df2ba3bf1
sajivekumar <148953828+sajivekumar@users.noreply.github.com>
2024-08-21 15:57:57 +0530

    Create test-tls.go

---
+++ test-tls.go
@@ -0,0 +1,116 @@
+// go build tls.go
+// ./tls sc-cert.pem sc-pkey.pem sc-ca_bundle.pem
+
+package main
+
+import (
+        "fmt"
+        "io"
+        "os"
+        "net"
+        "crypto/tls"
+        "crypto/x509"
+        "io/ioutil"
+        "log"
+)
+
+func sslVerifyCertificateAuthority(client *tls.Conn, tlsConf *tls.Config) {
+        err := client.Handshake()
+        if err != nil {
+                panic(err)
+        }
+        certs := client.ConnectionState().PeerCertificates
+        opts := x509.VerifyOptions{
+                DNSName:       client.ConnectionState().ServerName,
+                Intermediates: x509.NewCertPool(),
+                Roots:         tlsConf.RootCAs,
+        }
+        for i, cert := range certs {
+                if i == 0 {
+                        continue
+                }
+                opts.Intermediates.AddCert(cert)
+        }
+        _, err = certs[0].Verify(opts)
+        if err != nil {
+                panic(err)
+        }
+}
+
+func main() {
+
+        var conn net.Conn
+
+        hostname := "hci-omega-k8s-01-envoy.hciiwf.nimblestorage.com"
+        port := "443"
+        //URL := "https://hci-omega-k8s-01-envoy.hciiwf.nimblestorage.com:443"
+        // cert="/etc/3par/certs/manufacturing/storage-central-cert.pem"
+        // key="/etc/3par/certs/manufacturing/storage-central-pkey.pem"
+        // caCert:="/opt/tpd/mgmt_files/trustcerts/storagecentral-server-ca_bundle.pem"
+
+        mycert := os.Args[1]
+        mykey := os.Args[2]
+        ca_cert := os.Args[3]
+
+        cert, err := tls.LoadX509KeyPair(mycert, mykey )
+
+        if err != nil {
+                log.Fatal(err)
+        }
+
+        // Create a CA certificate pool and add cert.pem to it
+        caCert, err := ioutil.ReadFile(ca_cert)
+        if err != nil {
+                log.Fatal(err)
+        }
+
+        clientCertPool := x509.NewCertPool()
+        clientCertPool.AppendCertsFromPEM(caCert)
+
+        tlsConfig := &tls.Config{
+                ClientAuth:               tls.RequireAndVerifyClientCert,
+                Certificates:             []tls.Certificate{cert},
+                RootCAs:                  clientCertPool,
+                ClientCAs:                clientCertPool,
+                PreferServerCipherSuites: true,
+                MinVersion:               tls.VersionTLS12,
+        }
+
+        tlsConfig.ServerName = hostname
+
+        conn,err = net.Dial("tcp", hostname+":"+port)
+
+        fmt.Printf("Dialing to %s : %s\n", hostname, conn)
+
+        client := tls.Client(conn, tlsConfig)
+        if err := client.Handshake(); err != nil {
+            conn.Close()
+            fmt.Printf("ERROR: Handshake FAILED : %s\n", err)
+            switch err.(type) {
+                case x509.UnknownAuthorityError:
+                        fmt.Printf("x509.UnknownAuthorityError\n")
+                     log.Fatal(err)
+                case x509.CertificateInvalidError:
+                        fmt.Printf("x509.CertificateInvalidError\n")
+                     log.Fatal(err)
+                default:
+                     fmt.Printf("Default \n")
+                     log.Fatal(err)
+             }
+        } else {
+            server_certs := client.ConnectionState().PeerCertificates
+            fmt.Printf("Subject: %s\n", server_certs[0].Subject)
+            fmt.Printf("Issuer: %s\n", server_certs[0].Issuer)
+            fmt.Printf("==============================\n")
+            verified_chains := client.ConnectionState().VerifiedChains
+            fmt.Printf("%s\n", verified_chains[0])
+
+            buf, err := io.ReadAll(client)
+            if err != nil {
+               log.Fatal(err)
+            }
+            fmt.Printf("==> %s\n", buf)
+        }
+
+        client.Close()
+}
